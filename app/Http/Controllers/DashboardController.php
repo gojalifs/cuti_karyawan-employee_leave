c3159efe53147e670973e5a5f7543133a1c918ac
gojali99 <fajarsidik1999@gmail.com>
2023-11-17 01:53:39 +0700

    changed table and footer text

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,33 +12,33 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        
+
         $permohonan = DB::table('users')
-            ->join('permohonan_cuti','users.id','=','permohonan_cuti.user_id')
-            ->select('users.name','permohonan_cuti.id','permohonan_cuti.alasan_cuti','permohonan_cuti.tgl_mulai','permohonan_cuti.tgl_akhir','permohonan_cuti.status')
-            ->where('permohonan_cuti.status','pending')
+            ->join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
+            ->select('users.name', 'permohonan_cuti.id', 'permohonan_cuti.alasan_cuti', 'permohonan_cuti.tgl_mulai', 'permohonan_cuti.tgl_akhir', 'permohonan_cuti.status')
+            ->where('permohonan_cuti.status', 'pending')
             ->limit(5)
             ->get();
         $jmlPermohonan = Permohonan_Cuti::where('status', 'pending')->get()->count();
         $jmlPermohonanDisetujui = Permohonan_Cuti::where('status', 'disetujui')->get()->count();
         $jmlPermohonanDitolak = Permohonan_Cuti::where('status', 'ditolak')->get()->count();
-        
-        
-        return view('pages.Dashboard.DashboardAdmin',["permohonan" => $permohonan,"jmlPermohonan" => $jmlPermohonan,'jmlPermohonanDisetujui' => $jmlPermohonanDisetujui,'jmlPermohonanDitolak' => $jmlPermohonanDitolak]);
+
+
+        return view('pages.Dashboard.DashboardAdmin', ["permohonan" => $permohonan, "jmlPermohonan" => $jmlPermohonan, 'jmlPermohonanDisetujui' => $jmlPermohonanDisetujui, 'jmlPermohonanDitolak' => $jmlPermohonanDitolak]);
     }
     public function show()
     {
-        $id=Auth::user()->id;
+        $id = Auth::user()->id;
         $permohonan = DB::table('users')
-        ->join('permohonan_cuti','users.id','=','permohonan_cuti.user_id')
-        ->select('users.name','permohonan_cuti.alasan_cuti','permohonan_cuti.tgl_mulai','permohonan_cuti.tgl_akhir','permohonan_cuti.status')
-        ->where('users.id',$id)
-        ->limit(5)
-        ->get();
-        $sisaCuti = DB::table('karyawan')->select('jumlah_cuti')->where('user_id',$id)->first();
-        $jmlPermohonanDisetujui = Permohonan_Cuti::Where('status', 'disetujui')->where('user_id',$id)->get()->count();
-        $jmlPermohonanDitolak = Permohonan_Cuti::where('status', 'ditolak')->where('user_id',$id)->get()->count();
-       
-        return view('pages.Dashboard.DashboardKaryawan',["permohonan" => $permohonan,'sisa_cuti' => $sisaCuti->jumlah_cuti,'jmlPermohonanDisetujui' => $jmlPermohonanDisetujui,'jmlPermohonanDitolak' =>$jmlPermohonanDitolak]);
+            ->join('permohonan_cuti', 'users.id', '=', 'permohonan_cuti.user_id')
+            ->select('users.name', 'permohonan_cuti.alasan_cuti', 'permohonan_cuti.tgl_mulai', 'permohonan_cuti.tgl_akhir', 'permohonan_cuti.status')
+            ->where('users.id', $id)
+            ->limit(5)
+            ->get();
+        $sisaCuti = DB::table('users')->select('jumlah_cuti')->where('id', $id)->first();
+        $jmlPermohonanDisetujui = Permohonan_Cuti::Where('status', 'disetujui')->where('user_id', $id)->get()->count();
+        $jmlPermohonanDitolak = Permohonan_Cuti::where('status', 'ditolak')->where('user_id', $id)->get()->count();
+
+        return view('pages.Dashboard.DashboardKaryawan', ["permohonan" => $permohonan, 'sisa_cuti' => $sisaCuti->jumlah_cuti, 'jmlPermohonanDisetujui' => $jmlPermohonanDisetujui, 'jmlPermohonanDitolak' => $jmlPermohonanDitolak]);
     }
 }
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2019 <div class="bullet"></div> Design By <a href="#">Redstar</a>
+          Copyright &copy; 2019 <div class="bullet"></div> PT Nitigura Indonesia
         </div>
         <div class="footer-right">
         </div>
