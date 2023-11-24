2318c974eb72762770ab314244c742c94e60dac0
gojali99 <fajarsidik1999@gmail.com>
2023-11-24 21:30:33 +0700

    changed desctiption text

--- resources/views/pages/permohonanCuti/karyawan.blade.php
+++ resources/views/pages/permohonanCuti/karyawan.blade.php
@@ -60,7 +60,7 @@
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="formModal">Form Tambah Data Karyawan</h5>
+                <h5 class="modal-title" id="formModal">Form Pengajuan Cuti Karyawan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
